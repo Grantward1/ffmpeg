aeb611c39cc1a8812caadcb5ad2eaa3579cad9f4
Grant Ward <private@fakeemail.com>
2026-06-21 21:40:18 -0600

    Add licensing to script/readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # ffmpeg
 Useful scripts for ffmpeg
 
+## License
+These scripts are licensed under the GNU AGPL v3 only. See included file: `LICENSE` for full terms.
+
 ## Usage
 1. Clone this repo
 2. Initialize submodules:
@@ -10,3 +13,9 @@ Useful scripts for ffmpeg
     ```
 3. (optional) Add this directory to the path
 4. Use scripts
+
+## Included Scripts
+Each script should be fairly self explanatory to use. Run `<script.sh> --help` for information on that script's command line arguments.
+
+### crop.sh
+Uses ffmpeg to crop a video. See 
--- crop.sh
+++ crop.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#Provides a simple interface to use FFMPEG to crop a video.
+#Copyright (C) 2026 Grant Ward
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published
+#by the Free Software Foundation, version 3 only.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
